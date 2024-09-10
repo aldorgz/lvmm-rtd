ade1f7bb3f4f7d289b378c873c45e79a60e540b8
Aldo Rodriguez Guerrero <146978784+aldorgz@users.noreply.github.com>
2024-09-10 09:28:05 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LVMM'
+copyright = '2024, Aldo Guerrero Rodríguez Guerrero'
+author = 'Aldo Gerardo Rodríguez Guerrero'
 
 release = '0.1'
 version = '0.1.0'
